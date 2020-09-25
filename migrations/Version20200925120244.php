bfa2ea9da21fd6791a6a266c6b5f9b36ab3e1132
TuncayBasak25 <tuncay.basak@outlook.com>
2020-09-25 14:03:56 +0200

    Entities

---
+++ migrations/Version20200925120244.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20200925120244 extends AbstractMigration
+{
+    public function getDescription() : string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema) : void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE book ADD writting_date DATE DEFAULT NULL, ADD edition_date DATE DEFAULT NULL, ADD title VARCHAR(255) DEFAULT NULL');
+    }
+
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE book DROP writting_date, DROP edition_date, DROP title');
+    }
+}
